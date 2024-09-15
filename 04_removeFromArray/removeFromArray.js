0734016340231c72b0290df27ba6164f6072ae1b
amit-bharat <amitnit91@gmail.com>
2024-09-15 21:35:29 +0530

    Array exercise 4,5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,14 @@
-const removeFromArray = function(arr, ...items) {
-    if(arr.length===0 || args.length===0) return arr;
-    items.map((val,arr)=>{
-        let i=0;
-        while(i<=arr.length-1){
-            if(val===arr[i]){
-                arr.splice(i,1);
+const removeFromArray = function (arr, ...items) {
+    if (arr.length === 0 || items.length === 0) return arr;
+    items.map((value) => {
+        for (let i = 0; i < arr.length; i++) {
+            if (value === arr[i]) {
+                arr.splice(i, 1);
+                i--;
             }
         }
-
     });
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let sum=0;
+    if(Number.isInteger(num1) && Number.isInteger(num2)&&(num1>0 &&num2>0) ){
+        if(num1>num2){
+            // let temp=num1;
+            // num1=num2;
+            // num2=temp;
+            [num1,num2]=[num2,num1];
+        }
 
+        for(num1;num1<=num2;num1++){
+            sum+=num1;
+        }
+        return sum;
+    }
+    else{
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
