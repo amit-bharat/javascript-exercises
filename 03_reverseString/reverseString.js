a40f95febc6387c3ded7e510ad46a5bad29229fe
amit-bharat <amitnit91@gmail.com>
2024-09-15 14:01:19 +0530

    array exercise 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let str=word.split('').reverse().toString();
+    let result='';
+    for(let i=0;i<str.length;i++){
+        if(i%2===0){
+            result+=str.at(i);
+        }
+    }
+    return result;
 
+    //return word.split('').reverse().join(''); 
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    if(arr.length===0 || args.length===0) return arr;
+    items.map((val,arr)=>{
+        let i=0;
+        while(i<=arr.length-1){
+            if(val===arr[i]){
+                arr.splice(i,1);
+            }
+        }
+
+    });
 };
 
 // Do not edit below this line
